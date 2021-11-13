57f5c058bea19eadc6fffb0c1ae6369b52293955
Hiroshi Ohno <hohno@panopto.com>
2021-11-13 07:41:41 -0800

    merge from private Dev repository for 3.1.0 release

--- PanoptoRRLightService/Properties/AssemblyInfo.cs
+++ PanoptoRRLightService/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Panopto RR Light Service")]
-[assembly: AssemblyCopyright("Copyright © 2018 Panopto, Swivl, and University of Washington")]
+[assembly: AssemblyCopyright("Copyright © 2021 Panopto, Swivl, and University of Washington")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.1")]
-[assembly: AssemblyFileVersion("3.0.1")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
